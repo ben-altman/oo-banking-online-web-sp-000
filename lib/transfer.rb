bb950f8973aa1c7b86746fc4bef40e4e03335197
Ben Altman <altmanguitar@gmail.com>
2021-01-23 22:51:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,8 +29,8 @@ class Transfer
 
   def reverse_transfer
     if @status = "complete"
-      sender.balance += amount
-      receiver.balance -= amount
+      sender.balance += @amount
+      receiver.balance -= @amount
     end
   end
 end
